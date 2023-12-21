77f9e9020d92f8f2ab83aa3540a953fa11018ab5
Salah Bendary <salahbendary1@gmail.com>
2023-12-21 13:47:43 +0200

    Update config.h

--- VehicleControlSystem_Code/config.h
+++ VehicleControlSystem_Code/config.h
@@ -1,13 +1,15 @@
-/*
- * config.h
- *
- *  Created on: 20 Dec 2023
- *      Author: HD
+ /*
+ ============================================================================
+ Name        : config.h
+ Author      : Salah Bendary
+ Description : Configuration File of the project
+ Date        : 20/12/2023
+ ============================================================================
  */
 
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define WITH_ENGINE_TEMP_CONTROLLER 1
+#define WITH_ENGINE_TEMP_CONTROLLER 1 /* Enable or disable the Engine Temp Controller */
 
 #endif /* CONFIG_H_ */
