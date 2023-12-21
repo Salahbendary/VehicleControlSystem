a57298d723bfb556278363df25499ffeda654c0b
Salah Bendary <salahbendary1@gmail.com>
2023-12-21 13:47:48 +0200

    Update SensorFunctions.c

--- VehicleControlSystem_Code/SensorFunctions.c
+++ VehicleControlSystem_Code/SensorFunctions.c
@@ -9,7 +9,14 @@
 
 #include "SensorFunctions.h"
 
-
+/**
+ * @brief Function to select the traffic light based on user input.
+ * @param _ NO Paramaters
+ * @return (uint8)  Status of traffic light:
+ *          ('G') Green
+ *          ('O') Yellow
+ *          ('R') Red
+ */
 uint8 simulateTrafficLight() {
     printf("Enter the traffic light color (G, O, R): ");
     uint8 u8trafficLight;
@@ -17,7 +24,12 @@ uint8 simulateTrafficLight() {
     return u8trafficLight;
 }
 
-
+/**
+ * @brief Function to select the room temperature based on user input.
+ * @param _ NO Paramaters
+ * @return (uint32)  degree of room temperature:
+ *          integer value of room temperature
+ */
 uint32 simulateuRoomTemperature() {
     printf("Enter the room temperature: ");
     uint32 u32roomTemperature;
@@ -25,6 +37,13 @@ uint32 simulateuRoomTemperature() {
     return u32roomTemperature;
 }
 
+/**
+ * @brief Function to select the engine temperature based on user input.
+ * @param _ NO Paramaters
+ * @return (uint32)  degree of engine temperature:
+ *          integer value of engine temperature
+ *          (conditional based on WITH_ENGINE_TEMP_CONTROLLER).
+ */
 #if WITH_ENGINE_TEMP_CONTROLLER
 uint32 simulateEngineTemperature() {
     printf("Enter the engine temperature: ");
