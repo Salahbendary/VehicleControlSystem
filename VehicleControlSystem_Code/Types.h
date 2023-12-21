d2b9361b6eb7e61e3d74b61a80656ae9391220c0
Salah Bendary <salahbendary1@gmail.com>
2023-12-21 13:47:57 +0200

    Update Types.h

--- VehicleControlSystem_Code/Types.h
+++ VehicleControlSystem_Code/Types.h
@@ -27,10 +27,14 @@ typedef enum {
 
 /* Status of Traffic Light: Green or Yellow or Red */
 typedef enum {
-	GREEN_LIGHT,  /* Traffic light is Green */
-	YELLOW_LIGHT, /* Traffic light is Yellow */
-	RED_LIGHT     /* Traffic light is Red */
-}traficLightStatus;
+    GREEN_LIGHT_LOWERCASE  = 'g',    /* Traffic light is Green in lowercase */
+    GREEN_LIGHT_UPPERCASE  = 'G',    /* Traffic light is Green in uppercase */
+    YELLOW_LIGHT_LOWERCASE = 'o',    /* Traffic light is Yellow in lowercase */
+    YELLOW_LIGHT_UPPERCASE = 'O',    /* Traffic light is Yellow in uppercase */
+    RED_LIGHT_LOWERCASE    = 'r',    /* Traffic light is Red in lowercase */
+    RED_LIGHT_UPPERCASE    = 'R'     /* Traffic light is Red in uppercase */
+} TrafficLightStatus;
+
 
 /* Status of Traffic Light: Green or Yellow or Red */
 typedef enum {
@@ -40,9 +44,9 @@ typedef enum {
 
 /* Status of vehicle Speed */
 typedef enum {
-    SPEED_0, /* 0 km/hr speed of vehicle */
-    SPEED_30, /* 30 km/hr speed of vehicle */
-    SPEED_100 /* 100 km/hr speed of vehicle */
+    SPEED_0   = 0,  /* 0 km/hr speed of vehicle */
+    SPEED_30  = 30, /* 30 km/hr speed of vehicle */
+    SPEED_100 = 100 /* 100 km/hr speed of vehicle */
 }vehicleSpeedStatus;
 
 /* Status of Room Temperature */
